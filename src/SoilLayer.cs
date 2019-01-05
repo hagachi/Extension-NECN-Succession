03688294cc6624d4b05a637f1501887be428a730
Robert Scheller <rschell@ncsu.edu>
2019-01-05 09:49:41 -0500

    Minor updates.

--- src/SoilLayer.cs
+++ src/SoilLayer.cs
@@ -69,67 +69,65 @@ namespace Landis.Extension.Succession.NECN
             {
 
                 double r = -0.008314; // gas constant
-                double SoilT = 10.0;  //TBD
-                double SoilMoisture = 5.0; // TBD
+                double SoilT = SiteVars.SoilTemperature[site];  
+                double SoilMoisture = SiteVars.SoilWaterContent[site]; 
                 double SOC = SiteVars.SOM1soil[site].Carbon;
                 double SON = SiteVars.SOM1soil[site].Nitrogen;
                 double DOC = 0.0020;
                 double DON = 0.0011;
                 double microbial_C = 1.9703;
                 double microbial_N = 0.1970;
-                double ec = 0.0339;  // WHAT IS EC??
+                double enzymatic_concentration = 0.0339;  
 
-                // seasonal DOC input
-                double A1 = 0.0005; //       #seasonal amplitude
-                double w1 = 2 * Math.PI / 4559;
-                double dref = 0.0005;     //#reference input
-                double t = 1.0;
-                double DOC_input = dref + A1 * Math.Sin(w1 * t - Math.PI / 2);  // # mg C cm-3 soil 
-                                                                                //double A2 = 0;            //#daily amplitude
-                                                                                //double w2 = 2 * Math.PI;
-                                                                                // double t = 1:4559; = MINUTES??
+                // Note: We are initially going to forgo DOC as it is a very minor component of overall C cycling
+                // seasonal DOC input:
+                //double A1 = 0.0005; //       #seasonal amplitude
+                //double w1 = 2 * Math.PI / 4559;
+                //double dref = 0.0005;     //#reference input
+                //double t = 1.0;
+                // double DOC_input = dref + A1 * Math.Sin(w1 * t - Math.PI / 2);  // # mg C cm-3 soil 
 
 
-                // fitted2009
-                double ea_dep = 64.34135015;    //p[1]  #activation energy of SOM depolymerization
-                double ea_upt = 60.26134704;    //p[2]  #activation energy of DOC uptake
-                double a_dep = 1.05598E+11;     //p[3]   #pre-exponential constant for SOM depolymerization
-                double a_upt = 1.07944E+11;     //p[4]  #pre-exponential constant for uptake
-                double frac = 0.000378981;      //p[5]  #fraction of unprotected SOM, Magill et al. 2000
-                                                // p[6] 0.000466501;  
-                                                // p[7] 0.000486085;
-                double cnl = 48.81375915;       //p[8] #C:N of litter
-                double cns = 28.089739;         //p[9] #C:N of soil
-                double cnm = 9.803885526;         //p[10] #C:N of microbial biomass
-                double cne = 2.857178317;       //p[11] #C:N of enzymes
-                double km_dep = 0.002467667;    //p[12] #half-saturation constant for SOM depolymerization
-                double km_upt = 0.289955018;    //p[13] #half-saturation constant for DOC uptake
-                double r_ecloss = 0.00098887;  //p[14]                     #enzyme turnover rate
-                double r_death = 0.000150496;    //p[15]                      #microbial turnover rate
-                double cue = 0.299595251;       //p[16]                          #carbon use efficiency
-                double a = 0.50853192;         //p[17]                            #proportion of enzyme pool acting on SOC
-                double pconst = 0.481045149;     //p[18]                       #proportion of assimilated C allocated to enzyme production
-                double qconst = 0.491011779;    //p[19]                       #proportion of assimilated N allocated to enzyme production
-                double mic_to_som = 0.493381459;//p[20]                   #fraction of dead microbial biomass allocated to SOM
-                double km_o2 = 0.115473482;     //p[21]                        #Michaelis constant for O2
-                double dgas = 1.631550734;      //p[22]                         #diffusion coefficient for O2 in air
-                double dliq = 3.135405232;      //p[23]                         #diffusion coefficient for unprotected SOM and DOM in liquid
-                double o2airfrac = 0.202587072; //p[24]                    #volume fraction of O2 in air
-                double bd = 0.75743956;        //p[25]                           #bulk density
-                double pd = 2.50156948;          //p[26]                           #particle density
-                double soilMoistureA = -1.92593874;            //p[27]
-                double soilMoistureB = 9.774504229;       //p[28]                     
-                double sat = 0.492526228;           //p[29]#saturation level
+                // fitted2009 NOTE:  p[n] indicates the field from the prior csv input file.
+                double ea_dep = 64.34135015;        //p[1]  #activation energy of SOM depolymerization
+                double ea_upt = 60.26134704;        //p[2]  #activation energy of DOC uptake
+                double a_dep = 1.05598E+11;         //p[3]   #pre-exponential constant for SOM depolymerization
+                double a_upt = 1.07944E+11;         //p[4]  #pre-exponential constant for uptake
+                double frac = 0.000378981;          //p[5]  #fraction of unprotected SOM, Magill et al. 2000
+                                                    // p[6] 0.000466501;  NOT USED
+                                                    // p[7] 0.000486085;  NOT USED
+                double cn_litter = 48.81375915;     //p[8] #C:N of litter (was: cnl)
+                double cn_DOCN = 28.089739;         //p[9] #C:N of soil (was: cns)
+                double cn_microbial = 9.803885526;  //p[10] #C:N of microbial biomass (was: cnm)
+                double cn_enzymes = 2.857178317;    //p[11] #C:N of enzymes (was: cne)
+                double km_dep = 0.002467667;        //p[12] #half-saturation constant for SOM depolymerization
+                double km_upt = 0.289955018;        //p[13] #half-saturation constant for DOC uptake
+                double r_ecloss = 0.00098887;       //p[14] #enzyme turnover rate
+                double r_death = 0.000150496;       //p[15] #microbial turnover rate
+                double c_use_efficiency = 0.299595251;           //p[16] #carbon use efficiency (was: cue)
+                double p_enz_SOC = 0.50853192;      //p[17] #proportion of enzyme pool acting on SOC (was: a)
+                double pconst = 0.481045149;        //p[18] #proportion of assimilated C allocated to enzyme production
+                double qconst = 0.491011779;        //p[19] #proportion of assimilated N allocated to enzyme production
+                double mic_to_som = 0.493381459;    //p[20] #fraction of dead microbial biomass allocated to SOM
+                double km_o2 = 0.115473482;         //p[21] #Michaelis constant for O2
+                double dgas = 1.631550734;          //p[22] #diffusion coefficient for O2 in air
+                double dliq = 3.135405232;          //p[23] #diffusion coefficient for unprotected SOM and DOM in liquid
+                double o2airfrac = 0.202587072;     //p[24] #volume fraction of O2 in air
+                double bulk_density = 0.75743956;   //p[25] #bulk density (was: bd)
+                double particle_density = 2.50156948;             //p[26] #particle density (was: pd)
+                double soilMoistureA = -1.92593874; //p[27]
+                double soilMoistureB = 9.774504229; //p[28]                     
+                double saturation = 0.492526228;    //p[29] #saturation level (was: sat)
 
                 // Calculated internally or otherwise
                 double litter_c = 20;
-                double litter_n = -litter_c / cnl;    //#litter N input to SOC
+                double litter_n = litter_c / cn_litter;    //#litter N input to SOC
                 double doc_input = -99.99;            //#litter C input to DOC
-                double don_input = -doc_input / cns;  //litter N input to DOC
+                double don_input = doc_input / cn_DOCN;  //litter N input to DOC
 
-                double porosity = 1 - bd / pd;                                              //calculate porosity
+                double porosity = 1 - bulk_density / particle_density;                                              //calculate porosity
                 double soilm = soilMoistureA + soilMoistureB * SoilMoisture;                //calculate soil moisture scalar
-                soilm = (soilm > sat) ? sat : soilm;                                        //set upper bound on soil moisture (saturation)
+                soilm = (soilm > saturation) ? saturation : soilm;                                        //set upper bound on soil moisture (saturation)
                 soilm = (soilm < 0.1) ? 0.1 : soilm;                                        //set lower bound on soil moisture
                 double o2 = dgas * o2airfrac * Math.Pow((porosity - soilm), (4.0 / 3.0));    //calculate oxygen concentration
                 double sol_soc = dliq * Math.Pow(soilm, 3) * frac * SOC;                     //calculate unprotected SOC
@@ -138,34 +136,34 @@ namespace Landis.Extension.Succession.NECN
                 double vmax_upt = a_upt * Math.Exp(-ea_upt / (r * (SoilT + 273)));          //calculate maximum depolymerization rate
 
                 double upt_c = microbial_C * vmax_upt * DOC / (km_upt + DOC) * o2 / (km_o2 + o2); //calculate DOC uptake
-                double cmin = upt_c * (1 - cue);                                            //calculate initial C mineralization
+                double cmin = upt_c * (1 - c_use_efficiency);                                            //calculate initial C mineralization
                 double upt_n = microbial_N * vmax_upt * DON / (km_upt + DON) * o2 / (km_o2 + o2); //calculate DON uptake
                 double death_c = r_death * Math.Pow(microbial_C, 2);                         //calculate density-dependent microbial C turnover
                 double death_n = r_death * Math.Pow(microbial_N, 2);                         //calculate density-dependent microbial N turnover
 
-                double enz_c = pconst * cue * upt_c;                                        //calculate potential enzyme C production
+                double enz_c = pconst * c_use_efficiency * upt_c;                                        //calculate potential enzyme C production
                 double enz_n = qconst * upt_n;                                              //calculate potential enzyme N production
-                double eprod = (enz_c / cne >= enz_n) ? enz_n : (enz_c / cne);              //calculate actual enzyme based on Liebig's Law
-                double growth_c = (1 - pconst) * (upt_c * cue) + enz_c - cne * eprod;       //calculate potential microbial biomass C growth
+                double eprod = (enz_c / cn_enzymes >= enz_n) ? enz_n : (enz_c / cn_enzymes);              //calculate actual enzyme based on Liebig's Law
+                double growth_c = (1 - pconst) * (upt_c * c_use_efficiency) + enz_c - cn_enzymes * eprod;       //calculate potential microbial biomass C growth
                 double growth_n = (1 - qconst) * upt_n + enz_n - eprod;                     //calculate potential microbial biomass N growth
-                double growth = (growth_c / cnm >= growth_n) ? growth_n : (growth_c / cnm); //calculate actual microbial biomass growth based on Liebig's Law of the minimum (Schimel & Weintraub 2003 SBB)
+                double growth = (growth_c / cn_microbial >= growth_n) ? growth_n : (growth_c / cn_microbial); //calculate actual microbial biomass growth based on Liebig's Law of the minimum (Schimel & Weintraub 2003 SBB)
 
-                double overflow = growth_c - cnm * growth;                                  //calculate overflow metabolism of C
+                double overflow = growth_c - cn_microbial * growth;                                  //calculate overflow metabolism of C
                 double nmin = growth_n - growth;                                            //calculate N mineralization
 
-                double dmic_c = cnm * growth - death_c;                                     //calculate change in microbial C pool
+                double dmic_c = cn_microbial * growth - death_c;                                     //calculate change in microbial C pool
                 double dmic_n = growth - death_n;                                           //calculate change in microbial N pool
 
-                double eloss = r_ecloss * ec;                                               //calculate enzyme turnover
+                double eloss = r_ecloss * enzymatic_concentration;                                               //calculate enzyme turnover
                 double dec = eprod - eloss;                                                 //calculate change in enzyme pool
 
-                double decom_c = vmax_dep * a * ec * sol_soc / (km_dep + sol_soc + ec);     //calculate depolymerization of SOC using ECA kinetics (Tang 2015 GMD)
-                double decom_n = vmax_dep * (1 - a) * ec * sol_son / (km_dep + sol_son + ec); //calculate depolymerization of SON using ECA kinetics 
+                double decom_c = vmax_dep * p_enz_SOC * enzymatic_concentration * sol_soc / (km_dep + sol_soc + enzymatic_concentration);     //calculate depolymerization of SOC using ECA kinetics (Tang 2015 GMD)
+                double decom_n = vmax_dep * (1 - p_enz_SOC) * enzymatic_concentration * sol_son / (km_dep + sol_son + enzymatic_concentration); //calculate depolymerization of SON using ECA kinetics 
 
 
                 double dsoc = litter_c + death_c * mic_to_som - decom_c;                    //calculate change in SOC pool
                 double dson = litter_n + death_n * mic_to_som - decom_n;                    //calculate change in SON pool
-                double ddoc = doc_input + decom_c + death_c * (1 - mic_to_som) + cne * eloss - upt_c; //calculate change in DOC pool
+                double ddoc = doc_input + decom_c + death_c * (1 - mic_to_som) + cn_enzymes * eloss - upt_c; //calculate change in DOC pool
                 double ddon = don_input + decom_n + death_n * (1 - mic_to_som) + eloss - upt_n; //calculate change in DON pool
                 double dcout = cmin + overflow;                                             //calculate C efflux
 
