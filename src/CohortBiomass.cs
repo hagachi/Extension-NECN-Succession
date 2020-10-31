de3497f1762abace0a14b6d1709975669574f1d8
hagachi <chihiro.haga@ge.see.eng.osaka-u.ac.jp>
2020-10-31 18:31:45 +0900

    Track siteLAI and siteLAItree in calibrate log

--- src/CohortBiomass.cs
+++ src/CohortBiomass.cs
@@ -230,7 +230,7 @@ namespace Landis.Extension.Succession.NECN
                 Outputs.CalibrateLog.Write("{0},", cohort.EstablishedLoc);
                 //Outputs.CalibrateLog.Write("{0:0.00},{1:0.00},{2:0.00},{3:0.00}, {4:0.00},", limitLAI, limitH20, limitT, limitCapacity, limitN);
                 Outputs.CalibrateLog.Write("{0:0.00},{1:0.00},{2:0.00},{3:0.00},{4:0.00},", limitLAI, limitH20, limitT, limitN, competition_limit); // Chihiro 2020.02.03
-                Outputs.CalibrateLog.Write("{0:0.00},{1:0.00}", SiteVars.MonthlyLAITree[site][Main.Month], SiteVars.MonthlyLAI[site][Main.Month]); // Chihiro 2020.10.31
+                Outputs.CalibrateLog.Write("{0:0.00},{1:0.00}", SiteVars.MonthlyLAITree[site], SiteVars.MonthlyLAI[site]);
                 Outputs.CalibrateLog.Write("{0},{1},{2},{3:0.0},{4:0.0},", maxNPP, maxBiomass, (int)siteBiomass, (cohort.WoodBiomass + cohort.LeafBiomass), SiteVars.SoilTemperature[site]);
                 Outputs.CalibrateLog.Write("{0:0.00},{1:0.00},", woodNPP, leafNPP);
             }
